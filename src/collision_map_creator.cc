35aa406ddf0e1305fb0caf33f892ce548fcd0698
Valerio Magnago <valerio.magnago@hotmail.it>
2020-06-22 15:59:37 +0200

    feat: Yaml generation
    Now the yaml file is generate in parallel with pgm from the plugin

--- src/collision_map_creator.cc
+++ src/collision_map_creator.cc
@@ -1,27 +1,25 @@
-#define png_infopp_NULL (png_infopp)NULL
+#define png_infopp_NULL (png_infopp) NULL
 #define int_p_NULL (int*)NULL
 
-#include <fstream>
-#include <iostream>
 #include <math.h>
-#include <boost/gil/gil_all.hpp>
 #include <boost/gil/extension/io/png_dynamic_io.hpp>
+#include <boost/gil/gil_all.hpp>
 #include <boost/shared_ptr.hpp>
-#include <sdf/sdf.hh>
+#include <fstream>
 #include <ignition/math/Vector3.hh>
+#include <iostream>
+#include <sdf/sdf.hh>
 
-#include "gazebo/gazebo.hh"
+#include "collision_map_request.pb.h"
 #include "gazebo/common/common.hh"
+#include "gazebo/gazebo.hh"
 #include "gazebo/msgs/msgs.hh"
 #include "gazebo/physics/physics.hh"
 #include "gazebo/transport/transport.hh"
-#include "collision_map_request.pb.h"
 
 namespace gazebo
 {
-typedef const boost::shared_ptr<
-  const collision_map_creator_msgs::msgs::CollisionMapRequest>
-    CollisionMapRequestPtr;
+typedef const boost::shared_ptr<const collision_map_creator_msgs::msgs::CollisionMapRequest> CollisionMapRequestPtr;
 
 class CollisionMapCreator : public WorldPlugin
 {
@@ -30,43 +28,40 @@ class CollisionMapCreator : public WorldPlugin
   transport::SubscriberPtr commandSubscriber;
   physics::WorldPtr world;
 
-  public: void Load(physics::WorldPtr _parent, sdf::ElementPtr _sdf)
+public:
+  void Load(physics::WorldPtr _parent, sdf::ElementPtr _sdf)
   {
     node = transport::NodePtr(new transport::Node());
     world = _parent;
     // Initialize the node with the world name
     node->Init(world->Name());
-    std::cout << "Subscribing to: " << "~/collision_map/command" << std::endl;
-    commandSubscriber = node->Subscribe("~/collision_map/command",
-      &CollisionMapCreator::create, this);
+    std::cout << "Subscribing to: "
+              << "~/collision_map/command" << std::endl;
+    commandSubscriber = node->Subscribe("~/collision_map/command", &CollisionMapCreator::create, this);
     imagePub = node->Advertise<msgs::Image>("~/collision_map/image");
   }
 
-  public: void create(CollisionMapRequestPtr &msg)
+public:
+  void create(CollisionMapRequestPtr& msg)
   {
     std::cout << "Received message" << std::endl;
 
-    std::cout << "Creating collision map with corners at (" <<
-      msg->upperleft().x() << ", " << msg->upperleft().y() << "), (" <<
-      msg->upperright().x() << ", " << msg->upperright().y() << "), (" <<
-      msg->lowerright().x() << ", " << msg->lowerright().y() << "), (" <<
-      msg->lowerleft().x() << ", " << msg->lowerleft().y() <<
-        ") with collision projected from z = " <<
-      msg->height() << "\nResolution = " << msg->resolution() << " m\n" <<
-        "Occupied spaces will be filled with: " << msg->threshold() <<
-        std::endl;
+    std::cout << "Creating collision map with corners at (" << msg->upperleft().x() << ", " << msg->upperleft().y()
+              << "), (" << msg->upperright().x() << ", " << msg->upperright().y() << "), (" << msg->lowerright().x()
+              << ", " << msg->lowerright().y() << "), (" << msg->lowerleft().x() << ", " << msg->lowerleft().y()
+              << ") with collision projected from z = " << msg->height() << "\nResolution = " << msg->resolution()
+              << " m\n"
+              << "Occupied spaces will be filled with: " << msg->threshold() << std::endl;
 
     double dX_vertical = msg->upperleft().x() - msg->lowerleft().x();
     double dY_vertical = msg->upperleft().y() - msg->lowerleft().y();
-    double mag_vertical =
-      sqrt(dX_vertical * dX_vertical + dY_vertical * dY_vertical);
+    double mag_vertical = sqrt(dX_vertical * dX_vertical + dY_vertical * dY_vertical);
     dX_vertical = msg->resolution() * dX_vertical / mag_vertical;
     dY_vertical = msg->resolution() * dY_vertical / mag_vertical;
 
     double dX_horizontal = msg->upperright().x() - msg->upperleft().x();
     double dY_horizontal = msg->upperright().y() - msg->upperleft().y();
-    double mag_horizontal =
-      sqrt(dX_horizontal * dX_horizontal + dY_horizontal * dY_horizontal);
+    double mag_horizontal = sqrt(dX_horizontal * dX_horizontal + dY_horizontal * dY_horizontal);
     dX_horizontal = msg->resolution() * dX_horizontal / mag_horizontal;
     dY_horizontal = msg->resolution() * dY_horizontal / mag_horizontal;
 
@@ -75,13 +70,12 @@ class CollisionMapCreator : public WorldPlugin
 
     if (count_vertical == 0 || count_horizontal == 0)
     {
-      std::cout << "Image has a zero dimensions, check coordinates"
-                << std::endl;
+      std::cout << "Image has a zero dimensions, check coordinates" << std::endl;
       return;
     }
-    double x,y;
+    double x, y;
 
-    boost::gil::gray8_pixel_t fill(255-msg->threshold());
+    boost::gil::gray8_pixel_t fill(255 - msg->threshold());
     boost::gil::gray8_pixel_t blank(255);
     boost::gil::gray8_image_t image(count_horizontal, count_vertical);
 
@@ -93,8 +87,7 @@ class CollisionMapCreator : public WorldPlugin
 
     gazebo::physics::PhysicsEnginePtr engine = world->Physics();
     engine->InitForThread();
-    gazebo::physics::RayShapePtr ray =
-      boost::dynamic_pointer_cast<gazebo::physics::RayShape>(
+    gazebo::physics::RayShapePtr ray = boost::dynamic_pointer_cast<gazebo::physics::RayShape>(
         engine->CreateShape("ray", gazebo::physics::CollisionPtr()));
 
     std::cout << "Rasterizing model and checking collisions" << std::endl;
@@ -102,8 +95,7 @@ class CollisionMapCreator : public WorldPlugin
 
     for (int i = 0; i < count_vertical; ++i)
     {
-      std::cout << "Percent complete: " << i * 100.0 / count_vertical
-                << std::endl;
+      std::cout << "Percent complete: " << i * 100.0 / count_vertical << std::endl;
       x = i * dX_vertical + msg->lowerleft().x();
       y = i * dY_vertical + msg->lowerleft().y();
       for (int j = 0; j < count_horizontal; ++j)
@@ -119,7 +111,7 @@ class CollisionMapCreator : public WorldPlugin
         ray->GetIntersection(dist, entityName);
         if (!entityName.empty())
         {
-          image._view(i,j) = fill;
+          image._view(i, j) = fill;
         }
       }
     }
@@ -129,15 +121,18 @@ class CollisionMapCreator : public WorldPlugin
     {
       boost::gil::gray8_view_t view = image._view;
 
-      // Write to png 
-      // boost::gil::png_write_view(msg->filename(), view); 
+      // Write to png
+      // boost::gil::png_write_view(msg->filename(), view);
       // Write to pgm (pnm p2)
       pgm_write_view(msg->filename(), view);
+
+      yaml_write(msg->filename(), msg->lowerleft().x(), msg->lowerleft().y(), msg->resolution());
     }
     std::cout << "Output location: " << msg->filename() << std::endl;
   }
 
-  public: void pgm_write_view(const std::string& filename, boost::gil::gray8_view_t& view)
+public:
+  void pgm_write_view(const std::string& filename, boost::gil::gray8_view_t& view)
   {
     // Write image to pgm file
     std::cout << "running" << std::endl;
@@ -145,12 +140,14 @@ class CollisionMapCreator : public WorldPlugin
     int w = view.width();
 
     std::ofstream ofs;
-    ofs.open(filename+".pgm");
-    ofs << "P2" << '\n';          // grayscale
-    ofs << w << ' ' << h << '\n'; // width and height
-    ofs << 255 <<  '\n';          // max value
-    for (int y = 0; y < h; ++y){
-      for (int x = 0; x < w; ++x){
+    ofs.open(filename + ".pgm");
+    ofs << "P2" << '\n';           // grayscale
+    ofs << w << ' ' << h << '\n';  // width and height
+    ofs << 255 << '\n';            // max value
+    for (int y = 0; y < h; ++y)
+    {
+      for (int x = 0; x < w; ++x)
+      {
         // std::cout << (int)view(x, y)[0];
         ofs << (int)view(x, y)[0] << ' ';
       }
@@ -158,8 +155,34 @@ class CollisionMapCreator : public WorldPlugin
     }
     ofs.close();
   }
+
+  void yaml_write(const std::string& filename, const double& x_min, const double& y_min, const double& resolution)
+  {
+    auto pos = filename.rfind("/");
+    std::string map_name = ".pgm";
+
+    if (pos != std::string::npos)
+    {
+      map_name = filename.substr(pos + 1) + map_name;
+    }
+    else
+    {
+      map_name = filename + map_name;
+    }
+
+    std::ofstream ofs;
+    ofs.open(filename + ".yaml");
+
+    ofs << "image: " << map_name << std::endl;
+    ofs << "resolution: " << resolution << std::endl;
+    ofs << "origin: [" << x_min << ", " << y_min << ", 0.0]" << std::endl;
+    ofs << "occupied_thresh: 0.65" << std::endl;
+    ofs << "free_thresh: 0.35" << std::endl;
+    ofs << "negate: 0" << std::endl;
+    ofs.close();
+  }
 };
 
 // Register this plugin with the simulator
 GZ_REGISTER_WORLD_PLUGIN(CollisionMapCreator)
-}
+}  // namespace gazebo
